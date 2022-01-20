03d149c93f1850aaad3d52d7e0327eb20fda06f8
Jingbo Gao <gaojingbo0620@outlook.com>
2022-01-20 11:03:37 +0800

    update: baidu tongji

---
+++ docs/javascripts/baidu-tongji.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?55e68c9a93f3c1d411f56951d0a48992";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
